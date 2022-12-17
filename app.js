6e24b1e8c439b1b4277391d72da2202a00e7b26b
Pawel-Albert <wojciechowski.pawel.git@gmail.com>
2022-12-17 22:54:21 +0100

    some js logic to add remove and edit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+"use strict";
 const nameInputField = document.getElementById("name");
 const freshTodoForm = document.getElementById("todoForm");
 
 const userName = localStorage.getItem("userName") || "";
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
-
+const listElement = document.querySelector("#todoList");
 nameInputField.value = userName;
 nameInputField.addEventListener("change", (event) => {
   localStorage.setItem("userName", event.target.value);
@@ -34,50 +35,69 @@ freshTodoForm.addEventListener("submit", (event) => {
     category: category,
     done: false,
     creationTime: new Date().getTime(),
+    readonly: true,
   };
 
   todos = [...todos, todo];
   console.table(todos);
   localStorage.setItem("todos", JSON.stringify(todos));
   event.target.reset();
+  renderTodos();
 });
 
-const listElement = document.querySelector("#todoList");
-console.log(listElement);
-
-const test = `
-    <div class="todoItem">
-            <label>
-              <input type="checkbox" />
-              <span class="dot business"> </span>
-            </label>
-            <div class="todoText">
-              <input type="text" readonly value="Do some pussssshups" />
-            </div>
-            <div class="actionButtons">
-              <button class="edit">Edit</button>
-              <button class="delete">Delete</button>
-            </div>
-          </div>
-`;
-
-const test2 = `
+function getTodos() {
+  return JSON.parse(localStorage.getItem("todos"));
+}
+function markupTodos(data, index) {
+  let readonly;
+  data.readonly ? (readonly = "readonly") : (readonly = "");
+  return `
        <div class="todoItem">
             <label>
               <input type="checkbox" />
-              <span class="dot private"> </span>
+              <span class="dot ${data.category}"> </span>
             </label>
             <div class="todoText">
-              <input type="text" readonly value="Do some pushups222" />
+              <input type="text" ${readonly} index=${index} value="${data.subject}" />
             </div>
             <div class="actionButtons">
-              <button class="edit">Edit</button>
-              <button class="delete">Delete</button>
+              <button class="edit" index=${index} onclick="editTodo(this.index)">Edit</button>
+              <button class="delete" index=${index} onclick="deleteTodo(this.index)">Delete</button>
             </div>
           </div>
 `;
+}
+function renderTodos() {
+  const todos = getTodos();
+  listElement.innerHTML = "";
+  todos?.forEach((todo, index) => {
+    listElement.insertAdjacentHTML("afterbegin", markupTodos(todo, index));
+  });
+}
 
-listElement.insertAdjacentHTML("afterbegin", test);
-listElement.insertAdjacentHTML("afterbegin", test2);
-listElement.insertAdjacentHTML("afterbegin", test);
-listElement.insertAdjacentHTML("afterbegin", test2);
+function deleteTodo(index) {
+  todos = getTodos();
+  todos.splice(index, 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  renderTodos();
+}
+
+function editTodo(thisIndex) {
+  let taskIndex = thisIndex;
+  todos = getTodos();
+  todos?.forEach((_, index, todos) => {
+    if ((taskIndex = todos[index])) {
+      todos[index].readonly
+        ? (todos[index].readonly = false)
+        : (todos[index].readonly =
+            true &&
+            (todos[index].subject = document.querySelector(
+              `input[index='${index}']`
+            ).value));
+    }
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+  console.table(todos);
+  renderTodos();
+}
+renderTodos();
